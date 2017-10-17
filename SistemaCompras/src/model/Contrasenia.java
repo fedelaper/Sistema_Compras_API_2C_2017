79ca766ffb21926ad69a5f18af45314b61dec8c4
pgc89 <pgcassanelli1989@gmail.com>
2017-10-17 00:56:32 -0300

    Agrego validación de contraseña por regexp. Agrego unit tests de contrasenia.

--- SistemaCompras/src/model/Contrasenia.java
+++ SistemaCompras/src/model/Contrasenia.java
@@ -5,44 +5,48 @@ import java.util.Date;
 public class Contrasenia {
 
 	private String clave;
-	private String nuevaClave;
 	private Date fechaCreacion;
+	
+	public Contrasenia(String clave, Date fechaCreacion) {
+		super();
+		this.clave = clave;
+		this.fechaCreacion = fechaCreacion;
+	}
 
+	public Contrasenia() {
+		super();
+	}
+	
 	public void cambiarClave(String viejaClave, String nuevaClave) throws Exception{
-		if(this.clave.equals(viejaClave) && validar(nuevaClave)){
-			this.clave = nuevaClave;
-			return;
+		if(viejaClave.equals(this.clave)){
+			this.setClave(nuevaClave);
 		}else{
-			throw new Exception("Clave inválida.");
+			throw new Exception("Clave vieja ingresada no coincide con clave actual.");
 		}
 	}
 	
 	//ver si la clave esta vencida
-		public boolean estaVencida(){
-			long diff = (new Date().getTime() - fechaCreacion.getTime()) / (1000 * 60 * 60 * 24);
-			return diff > 180;
-		}
+	public boolean estaVencida(){
+		long diff = (new Date().getTime() - fechaCreacion.getTime()) / (1000 * 60 * 60 * 24);
+		return diff > 180;
+	}
 		
-		//TODO validacion de formato de clave
-		private boolean validar(String clave){
-			return true;
-		}
-
-	public String getNuevaClave() {
-		return nuevaClave;
+	//validacion de formato de clave
+	private boolean validar(String clave){
+		return clave.matches("^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z]).{8,20}$");
 	}
-
-	public void setNuevaClave(String nuevaClave) {
-		this.nuevaClave = nuevaClave;
-	}
-
 	
 	public String getClave() {
 		return clave;
 	}
 
-	public void setClave(String clave) {
-		this.clave = clave;
+	public void setClave(String clave) throws Exception {
+		if(validar(clave))
+		{
+			this.clave = clave;
+		}else{
+			throw new Exception("Clave inválida");
+		}
 	}
 
 	public Date getFechaCreacion() {
@@ -53,15 +57,5 @@ public class Contrasenia {
 		this.fechaCreacion = fechaCreacion;
 	}
 
-	public Contrasenia(String clave, String nuevaClave, Date fechaCreacion) {
-		super();
-		this.clave = clave;
-		this.nuevaClave = nuevaClave;
-		this.fechaCreacion = fechaCreacion;
-	}
-
-	public Contrasenia() {
-		super();
-	}
 	
 }
